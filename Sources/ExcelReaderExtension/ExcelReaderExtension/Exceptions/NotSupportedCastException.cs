36eab00b30137b82e81ae7f4ecfec09d64351c3a
Wachirawit.C <wachirawit.chankaew@gmail.com>
2017-04-19 12:43:35 +0700

    Create custom exceptions

---
+++ Sources/ExcelReaderExtension/ExcelReaderExtension/Exceptions/NotSupportedCastException.cs
@@ -0,0 +1,9 @@
+﻿namespace ExcelReaderExtension.Exceptions
+{
+    public class NotSupportedCastException : System.Exception
+    {
+        public NotSupportedCastException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Sources/ExcelReaderExtension/ExcelReaderExtension/Exceptions/ValidationErrorException.cs
@@ -0,0 +1,9 @@
+﻿namespace ExcelReaderExtension.Exceptions
+{
+    public class ValidationErrorException : System.Exception
+    {
+        public ValidationErrorException(string message) : base(message)
+        {
+        }
+    }
+}
