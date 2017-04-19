3ce4e791996926efd4d893814f76ef391bf53058
Wachirawit.C <wachirawit.chankaew@gmail.com>
2017-04-19 13:06:47 +0700

    Update date time parse

---
+++ Sources/ExcelReaderExtension/ExcelReaderExtension/Exceptions/Trap/ThrowIfs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExcelReaderExtension.Exceptions.Trap
+{
+    public static class ThrowIfs
+    {
+        public static void NullOrEmpty<T>(List<T> sources, string name)
+        {
+            if (sources == null || sources.Any() == false)
+            {
+                var message = $"{name} must not null or empty";
+                throw new ArgumentException(message);
+            }
+        }
+    }
+}
---
+++ Sources/ExcelReaderExtension/ExcelReaderExtension/Infrastructure/Parse/NullableDateTimeParse.cs
@@ -0,0 +1,34 @@
+﻿using ExcelReaderExtension.Exceptions.Trap;
+using ExcelReaderExtension.Infrastructure.Interface;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExcelReaderExtension.Infrastructure.Parse
+{
+    public class NullableDateTimeParse : IParse<DateTime?>
+    {
+        private readonly object value;
+        private readonly List<string> formats;
+
+        public NullableDateTimeParse(object value, List<string> formats)
+        {
+            ThrowIfs.NullOrEmpty(formats, nameof(formats));
+
+            this.value = value;
+            this.formats = formats;
+        }
+
+        public DateTime? Get()
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            DateTime outTime;
+            var isSuccess = DateTime.TryParseExact(value.ToString(), formats.ToArray(), CultureInfo.InvariantCulture, DateTimeStyles.None, out outTime);
+            return isSuccess == false ? (DateTime?)null : outTime;
+        }
+    }
+}
