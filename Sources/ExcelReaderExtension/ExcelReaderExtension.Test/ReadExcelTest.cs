40c2b0bdc308be71011f76ec8529f9ee684d46c2
Wachirawit.C <wachirawit.chankaew@gmail.com>
2017-04-19 13:44:39 +0700

    Update unit test.

---
+++ Sources/ExcelReaderExtension/ExcelReaderExtension.Test/ReadExcelTest.cs
@@ -0,0 +1,33 @@
+﻿using ExcelReaderExtension.Extensions;
+using NUnit.Framework;
+using OfficeOpenXml;
+using System.IO;
+
+namespace ExcelReaderExtension.Test
+{
+    [TestFixture]
+    public class ReadExcelTest
+    {
+        [Test]
+        public void Test_ReadInt()
+        {
+            const string filePath = @"E:\Source\ExcelReaderExtension\Sources\ExcelReaderExtension\ExcelReaderExtension.Test\DataSources\Examle1.xlsx";
+            var fileInfo = new FileInfo(filePath);
+
+            using (var package = new ExcelPackage(fileInfo))
+            {
+                var worksheet = package.Workbook.Worksheets[1];
+
+                var value = worksheet.Cells[1, 1]
+                    .Cast<int>()
+                    .NumericOnly()
+                        .WithMessage(cell => $"{cell.Address} is support numeric only")
+                    .Get();
+
+                const int expected = 1;
+
+                Assert.AreEqual(expected, value);
+            }
+        }
+    }
+}
--- Sources/ExcelReaderExtension/ExcelReaderExtension/Extensions/ExcelRangeExtension.cs
+++ Sources/ExcelReaderExtension/ExcelReaderExtension/Extensions/ExcelRangeExtension.cs
@@ -1,6 +1,44 @@
-﻿namespace ExcelReaderExtension.Extensions
+﻿using ExcelReaderExtension.Infrastructure;
+using ExcelReaderExtension.Infrastructure.Interface;
+using ExcelReaderExtension.Infrastructure.Parse;
+using OfficeOpenXml;
+using System;
+using System.Linq.Expressions;
+
+namespace ExcelReaderExtension.Extensions
 {
     public static class ExcelRangeExtension
     {
+        public static IRuleBuilder<T> Cast<T>(this ExcelRange range)
+        {
+            return Cast(range, new DefaultParse<T>(range, null, null));
+        }
+
+        public static IRuleBuilder<T> Cast<T>(this ExcelRange range, Expression<Func<ExcelRangeBase, string>> error)
+        {
+            return Cast(range, new DefaultParse<T>(range, null, error));
+        }
+
+        public static IRuleBuilder<T> Cast<T>(this ExcelRange range, Expression<Func<object, T>> function)
+        {
+            return Cast(range, new DefaultParse<T>(range, function, null));
+        }
+
+        public static IRuleBuilder<T> Cast<T>(this ExcelRange range, Expression<Func<object, T>> function, Expression<Func<ExcelRangeBase, string>> error)
+        {
+            return Cast(range, new DefaultParse<T>(range, function, error));
+        }
+
+        public static IRuleBuilder<T> Cast<T>(this ExcelRange range, Expression<Func<object, IParse<T>>> function)
+        {
+            var compile = function.Compile();
+
+            return Cast(range, compile(range.Value));
+        }
+
+        public static IRuleBuilder<T> Cast<T>(this ExcelRange range, IParse<T> parse)
+        {
+            return new RuleBuilder<T>(range, parse);
+        }
     }
 }
